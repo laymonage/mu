82b9f3ba6f5a8a46da152109c517d5f0e82d9ef0
sage <laymonage@gmail.com>
2020-10-31 18:33:31 +0700

    feat: predefine S and 0-9

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,16 @@ run as source =
       putStrLn $ intercalate " ; " $ map (T.unpack . prettyAST) res
       return as'
 
+
+runSilent :: Aliases -> T.Text -> Aliases
+runSilent as source =
+  case runParser program "repl" source of
+    Left _ -> as
+    Right exprs -> do
+      let (_, as') = runState (sequence $ map evaluate exprs) as
+      as'
+
+
 repl :: Aliases -> IO ()
 repl as = do
   putStr "> "
@@ -29,5 +39,27 @@ repl as = do
   as' <- run as $ T.pack input
   repl as'
 
+
+initialInputs :: [String]
+initialInputs =
+  [ "S := \\w.\\y.\\x.y(w y x)"
+  , "0 := \\s.\\z.z"
+  , "1 := S 0"
+  , "2 := S 1"
+  , "3 := S 2"
+  , "4 := S 3"
+  , "5 := S 4"
+  , "6 := S 5"
+  , "7 := S 6"
+  , "8 := S 7"
+  , "9 := S 8" ]
+
+initialAliases :: [T.Text]
+initialAliases = map T.pack initialInputs
+
+initialEnvironment :: Foldable t => t T.Text -> Aliases
+initialEnvironment = foldl runSilent M.empty
+
+
 main :: IO ()
-main = repl M.empty
+main = repl $ initialEnvironment initialAliases
